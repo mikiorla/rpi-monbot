382ed185eabb73ee38381621dcfd13d19151701c
Milan Orlovic <mikiorla@gmail.com>
2019-07-16 13:30:56 +0200

    working on ...

--- app2/app.py
+++ app2/app.py
@@ -2,8 +2,23 @@ import os
 import time
 import datetime
 from importlib import import_module
-from flask import Flask,render_template,Response
-import psutil #sudo pip3 install psutil
+from flask import Flask, render_template, Response
+import psutil  # sudo pip3 install psutil
+import RPi.GPIO as GPIO
+
+# GPIO.getmode()
+GPIO.setmode(GPIO.BOARD)
+# GPIO.RPI_INFO
+
+# chan_list = [11,13]
+# GPIO.setup(chan_list, GPIO.OUT)
+# GPIO.output(chan_list, GPIO.LOW)
+
+pins = {
+   11: {'name': 'GPIO 11', 'state': GPIO.LOW},
+   13: {'name': 'GPIO 13', 'state': GPIO.LOW}
+   }
+
 
 # import camera driver
 if os.environ.get('CAMERA'):
@@ -14,42 +29,55 @@ else:
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
 
+
 def measure_temp():
         temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=",""))
+        return (temp.replace("temp=", ""))
+
 
 def get_cpu_usage():
-        #return str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\))
+        # return str(os.popen("top -n1 | awk '/Cpu\(s\):/ {print $2}'").readline().strip(\))
         cpu_usage = psutil.cpu_percent()
         return (str(cpu_usage))
-        #return ("2")
-        
-app = Flask(__name__) 
+        # return ("2")
+
+
+app = Flask(__name__)
 @app.route("/")
 def index():
    now = datetime.datetime.now()
    dateStr = now.strftime("%Y-%m-%d")
    timeStr = now.strftime("%H:%M:%S")
    templateData = {
-      "title" : "RPi Infos",
+      "title": "RPi Infos",
       "date": dateStr,
       "timeJS": timeStr}
-   return render_template('t_index.html',**templateData)
-      
+   return render_template('t_index.html', **templateData)
+
+
 @app.route("/temp")
 def temp():
-   data=["Temperature","RPi temperature: ",measure_temp()]
-   return render_template('t_temp.html',data=data)
-    
+   data = ["Temperature", "RPi temperature: ", measure_temp()]
+   return render_template('t_temp.html', data=data)
+
+
 @app.route("/cpu")
 def cpu():
-    data=["CPU Page","CPU usage: ",get_cpu_usage()]
-    return render_template('t_cpu.html',data=data)
+    data = ["CPU Page", "CPU usage: ", get_cpu_usage()]
+    return render_template('t_cpu.html', data=data)
+
 
 @app.route('/stream')
 def stream():
-    """Video streaming home page."""
-    return render_template('stream2.html')
+   """Video streaming home page."""
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   
+   # Put the pin dictionary into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+   return render_template('stream2.html',**templateData)
 
 def gen(camera):
     """Video streaming generator function."""
@@ -64,6 +92,10 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+
 
 if __name__ == "__main__":
  app.run(host='0.0.0.0',debug=True,threaded=True)
